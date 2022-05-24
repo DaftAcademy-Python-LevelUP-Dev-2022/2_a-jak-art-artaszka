b0d24bb22e88bbcee101a144cf29f2c84a1a7c19
Artur <31184382+artaszka@users.noreply.github.com>
2022-05-24 03:07:23 +0200

    Update file.py
    update

--- file.py
+++ file.py
@@ -1,9 +1,31 @@
 def greeter(func):
-    pass
+    def inner(*args, **kwargs):
+        data_from_function = func(*args, **kwargs).title()
+        result = "Aloha " + data_from_function
+        return result
+    
+    return inner
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def inner(*args):
+        def suma(count):
+            how_many = 0
+            if str(count)[0] == "-":
+                for numbers in str(count)[1:]:
+                    how_many -= int(numbers)
+            else:
+                for numbers in str(count):
+                    how_many += int(numbers)
+            return how_many
+        my_list = func(*args).split()
+        num_one = suma(my_list[0])
+        num_two = suma(my_list[1])
+        if num_one == num_two:
+            return str(num_one) + ' == ' + str(num_two)
+        else:
+            return str(num_one) + ' != ' + str(num_two)
+    return inner
 
 
 def format_output(*required_keys):
@@ -11,4 +33,10 @@ def format_output(*required_keys):
 
 
 def add_method_to_instance(klass):
-    pass
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            return func()
+        setattr(klass, func.__name__, wrapper)
+        return wrapper
+    return decorator
+
